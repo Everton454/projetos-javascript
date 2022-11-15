098b721d00520eabb3f7702e82d1c72951469d7c
Everton Oliveira <112816785+Everton454@users.noreply.github.com>
2022-11-15 10:56:18 -0300

    commit typeof

--- test-JS.js
+++ test-JS.js
@@ -5,11 +5,24 @@ let nome = 'Palmeiras'
 console.log(nome)
 console.log(nome + ',o maior')
 
-var nome = 'Brazil'*/
+var nome = 'Brazil'
 
 
 let n1 = 10
 let n2 = 5 
 let n3 = 10
 
-console.log(n1 >= n3)
+ var ternario = 11 % 2 === 0 ? "par" : "impar";
+ console.log(ternario);*/
+
+ let v1 = 10
+ let v2 = "10"
+ let v3 = v1 === v2
+ let v4 = {nome : "Everton"};
+
+ console.log(typeof(v1));
+ console.log(typeof(v2));
+ console.log(typeof(v3));
+ console.log(typeof(v4));
+
+
