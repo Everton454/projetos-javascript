584fb853b8abb72d4e66c347e2b0950b55b219e3
Everton Oliveira <112816785+Everton454@users.noreply.github.com>
2022-11-15 09:35:26 -0300

    commit step

--- test-JS.js
+++ test-JS.js
@@ -3,6 +3,13 @@
 /*console.log('olá mundo!')
 let nome = 'Palmeiras'
 console.log(nome)
-console.log(nome + ',o maior')*/
+console.log(nome + ',o maior')
 
-var nome = 'Brazil'
+var nome = 'Brazil'*/
+
+
+let n1 = 10
+let n2 = 5 
+let n3 = 10
+
+console.log(n1 >= n3)
